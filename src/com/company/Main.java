67d84758e9db59e1356055874a1c31a659a862cc
MaestroMichael <zapart.michal7@gmail.com>
2019-10-14 22:01:47 +0200

    Clone repo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("First change");
+        System.out.println("Git Clone");
     }
 }
