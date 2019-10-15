90712960d0015c7c0e0cf46ded81f160dd6efc54
MaestroMichael <zapart.michal7@gmail.com>
2019-10-15 10:16:37 +0200

    Create new branch

---
+++ Desktop/zad51/src/main/java/com/example/zad51/Zad51Application.java
@@ -0,0 +1,13 @@
+package com.example.zad51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zad51Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zad51Application.class, args);
+	}
+
+}
