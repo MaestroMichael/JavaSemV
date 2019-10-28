08a959cbdecbf23a74a6857c4224ac09f46793cc
MaestroMichael <zapart.michal7@gmail.com>
2019-10-28 13:53:28 +0100

    Fixed changes

--- Desktop/PRiRThread/src/com/company/Main1.java
+++ Desktop/PRiRThread/src/com/company/Main1.java
@@ -7,31 +7,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 class Main {
-    static class MyThread implements Runnable{
-        int start;
-        public MyThread(int x){
-            start=x;
-        }
-        @Override
-        public void run() {
-            for (int i=start;i<=start+20;i++){
-                System.out.println(i);
-            }
-        }
-    }
     static class MyThread2 extends Thread{
         public Integer max;
         private Double salary=99999999.0;
-        private int minRange,maxRange;
+        private int firstCount,lastCount;
         HashMap<Integer, Double> data;
         public MyThread2(HashMap<Integer,Double>x,int min,int max){
             data=x;
-            minRange=min;
-            maxRange=max;
+            firstCount=min;
+            lastCount=max;
         }
         @Override
         public void run(){
-            for(int i=minRange;i<maxRange;i++){
+            for(int i=firstCount;i<lastCount;i++){
                 if(data.get(i)<salary){
                     max=i;
                     salary=data.get(i);
@@ -43,28 +31,29 @@ class Main {
         HashMap<Integer,Double> data=new HashMap<>();
         BufferedReader reader =new BufferedReader(new FileReader("dane.txt"));
         String line;
-        int k=1;
+        int howManyThreads=3;
         while((line = reader.readLine()) != null){
             String[] lines = line.split(";");
             data.put(Integer.parseInt(lines[0]),Double.parseDouble(lines[1]));
         }
         reader.close();
-        if(k==1){
+        if(howManyThreads==1){
             MyThread2 thr=new MyThread2(data,0,data.size());
             thr.start();
             thr.join();
+            System.out.println("K wynosi "+howManyThreads);
             System.out.println(thr.max+" "+thr.salary);
         }
         else{
             ArrayList<MyThread2> threads=new ArrayList<>();
-            for(int i=0;i<k;i++){
-                threads.add(new MyThread2(data,((i+1)*(data.size()/k))-(data.size()/k),((i+1)*(data.size()/k))));
+            for(int i=0;i<howManyThreads;i++){
+                threads.add(new MyThread2(data,((i+1)*(data.size()/howManyThreads))-(data.size()/howManyThreads),((i+1)*(data.size()/howManyThreads))));
                 threads.get(i).start();
             }
-            for(int i=0;i<k;i++)threads.get(i).join();
+            for(int i=0;i<howManyThreads;i++)threads.get(i).join();
             Double min=999999.0;
             Integer index = null;
-            for(int i=0;i<k;i++){
+            for(int i=0;i<howManyThreads;i++){
                 if(threads.get(i).salary<min){
                     min=threads.get(i).salary;
                     index=threads.get(i).max;
