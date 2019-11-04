091df2169efc1f12492759e142806a7897ddfc99
MaestroMichael <zapart.michal7@gmail.com>
2019-11-05 00:28:19 +0100

    Added Test3,4

--- src/main/java/com/example/demo/StudentService.java
+++ src/main/java/com/example/demo/StudentService.java
@@ -5,12 +5,22 @@ import io.vavr.collection.List;
 
 public class StudentService {
 
-    List<Student> getStudents() {
-        return List.empty();
+    List<Student> getStudents()
+    {
+//        return List.empty();
+        return this.students;
     }
-
+    int id=0;
     Student addStudent(NewStudent n_Student) {
-        return new Student(1,"aa","aa","aa");
+
+//        Student created=new Student(1,"Student1", "1-2-3", "IP");
+
+            id++;
+
+        Student created=new Student(id,n_Student.name, n_Student.numer,n_Student.grupa);
+        //return new Student(1,"aa","aa","aa");
+        students=students.prepend(created);
+        return created;
 
     }
 
--- src/test/java/com/example/demo/StudentServiceTest.java
+++ src/test/java/com/example/demo/StudentServiceTest.java
@@ -24,5 +24,20 @@ public class StudentServiceTest {
 
     }
 
+@Test
+    public void addStudentIsReturned(){
+    final StudentService service = new StudentService();
+    final Student created = service.addStudent(new NewStudent("Student1", "1-2-3", "IP"));
+    final List<Student> all = service.getStudents();
+    assertEquals("Student1",all.head().name);
+}
 
+@Test
+    public void addStudnetsHasNewId(){
+        final StudentService service=new StudentService();
+        final Student s1=service.addStudent(new NewStudent("S1","123","ip30"));
+        final Student s2=service.addStudent(new NewStudent("s2","234","ip30"));
+        assertNotEquals(s1.id,s2.id);
+        assertEquals(2,service.getStudents().size());
+}
 }
